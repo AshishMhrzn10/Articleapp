d9072a0155636055b4b8b853d12b1c7215f495ab
Ashish Maharjan <amaharjan1032@gmail.com>
2020-11-28 18:28:18 +0545

    Pug template done

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 var express = require("express");
-
+var path = require("path");
+//Init app
 var app = express();
 
+//Load view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+//Home route
 app.get("/", function (req, res) {
-	res.send("Hello World");
+	let articles = [
+		{
+			id: 1,
+			title: "Article 1",
+			author: "Ashish",
+			body: "This is test 1",
+		},
+		{
+			id: 2,
+			title: "Article 2",
+			author: "John Doe",
+			body: "This is test 2",
+		},
+		{
+			id: 3,
+			title: "Article 3",
+			author: "Ashish",
+			body: "This is test 3",
+		},
+	];
+	res.render("index", {
+		title: "Hello",
+		articles: articles,
+	});
 });
 
+//Add route
+app.get("/articles/add", function (req, res) {
+	res.render("add_article", {
+		title: "Add article",
+	});
+});
+
+//Start server
 app.listen(3000, function () {
 	console.log("Server started on port 3000...");
 });
