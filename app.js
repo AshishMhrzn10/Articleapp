9d2be7fbb173aacb277c8ba04a916c3fe5f54b9b
Ashish Maharjan <amaharjan1032@gmail.com>
2020-11-28 22:14:06 +0545

    Delete done

--- app.js
+++ app.js
@@ -82,6 +82,44 @@ app.get("/article/:id", function (req, res) {
 	});
 });
 
+//Load edit form
+app.get("/article/edit/:id", function (req, res) {
+	Article.findById(req.params.id, function (err, article) {
+		res.render("edit_article", {
+			article: article,
+		});
+	});
+});
+
+//Edit post article
+app.post("/articles/edit/:id", function (req, res) {
+	let article = {};
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+
+	let query = { _id: req.params.id };
+	Article.update(query, article, function (err) {
+		if (err) {
+			console.log(err);
+			return;
+		} else {
+			res.redirect("/");
+		}
+	});
+});
+
+//Delete
+app.delete("/article/:id", function (req, res) {
+	let query = { _id: req.params.id };
+	Article.remove(query, function (err) {
+		if (err) {
+			console.log(err);
+		}
+		res.send("Success");
+	});
+});
+
 //Start server
 app.listen(3000, function () {
 	console.log("Server started on port 3000...");
